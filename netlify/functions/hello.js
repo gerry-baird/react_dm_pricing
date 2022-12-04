4ebfc7d324ed9c19148d0fbbc3b0c7e5e8927553
Gerry Baird <gerry.baird@uk.ibm.com>
2022-12-04 15:33:49 +0000

    tweak

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,5 +1,7 @@
 exports.handler = async function (event, context) {
   console.log("Logged from hello function");
+  console.debug("Logged from hello function with debug");
+  console.log("## ENVIRONMENT VARIABLES: " + serialize(process.env));
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World2" }),
