7b7648ddc20c4b97ea0120cd3e9877525bcfcec0
Gerry Baird <gerry.baird@uk.ibm.com>
2022-12-04 15:04:58 +0000

    function update

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,6 +2,6 @@ exports.handler = async function (event, context) {
   console.log("Logged from hello function");
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World2" }),
   };
 };
