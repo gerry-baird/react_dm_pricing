4b9fd7c65c87e914ffda8cb207ce60646da6649b
Gerry Baird <gerry.baird@uk.ibm.com>
2022-12-04 15:39:39 +0000

    secret url

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async function (event, context) {
   console.log("Logged from hello function");
   console.debug("Logged from hello function with debug");
-  console.log("## ENVIRONMENT VARIABLES: " + serialize(process.env));
+  console.log("## ENVIRONMENT VARIABLES: " + process.env.SECRET_URL);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World2" }),
